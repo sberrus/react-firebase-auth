58b4cecdceaa431950f263502e53f92d4081125f
SamDev <samuelberrus@gmail.com>
2021-04-16 18:36:49 +0200

    .

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,21 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Login = () => {
+
+    const [email, setEmail] = useState("");
+    const [pass, setPass] = useState("");
+
+    const procesarDatos = (e) => {
+        e.preventDefault();
+        
+    }
     return (
         <div className="mt-5">
             <h3 className="display-5">Acceso o registro de usuarios</h3>
             <hr />
             <div className="row justify-content-center">
                 <div className="col-12 col-sm-8 col-md-6 col-xl-4">
-                    <form >
+                    <form onSubmit={procesarDatos}>
                         <input
                             type="email"
                             className="form-control mb-2"
-                            placeholder="Ingrese un email" />
+                            placeholder="Ingrese un email"
+                            onChange={e => setEmail(e.target.value)}
+                            value={email}
+                        />
                         <input
                             type="password"
                             className="form-control mb-2"
-                            placeholder="Ingresar contraseña" />
+                            placeholder="Ingresar contraseña"
+                            onChange={e => setPass(e.target.value)}
+                            value={pass}
+                        />
                         <button
                             className="btn btn-dark btn-block col-12 mb-1">
                             Registrarse
